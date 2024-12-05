828918574730e26cdcc28ab4a84e042e00312a2e
Trikaan <116668675+Trikaan@users.noreply.github.com>
2024-12-05 23:56:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const navLinks = document.querySelectorAll(".nav-link");
+
+        navLinks.forEach((link) => {
+            link.addEventListener("click", function () {
+                navLinks.forEach((lnk) => lnk.classList.remove("active"));
+                this.classList.add("active");
+            });
+        });
+    });
+
